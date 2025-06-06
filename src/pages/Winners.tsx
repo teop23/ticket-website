5b3e5090891f6d4b02623337af48d8dfe41c7cf3
teop21 <38569427+teop23@users.noreply.github.com>
2025-06-06 04:42:33 +0200

    Remove back to home button

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { WinnersTable } from '../components/WinnersTable';
 import { getAllWinners } from '../utils/winners';
-import { Link } from 'react-router-dom';
 
 export const Winners: React.FC = () => {
   const winners = getAllWinners();
   
   return (
     <div className="pt-16 bg-gray-50">
-      <div className="container mx-auto px-4 py-8">
-        <Link to="/" className="text-red-600 hover:text-red-800 font-medium inline-flex items-center">
-          ← Back to Home
-        </Link>
-      </div>
       <WinnersTable winners={winners} />
     </div>
   );
