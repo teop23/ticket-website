e2a88637e62e76887853b8b441860455d084fb95
teop21 <38569427+teop23@users.noreply.github.com>
2025-06-06 05:17:49 +0200

    Fix Winners page navigation scroll

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { WinnersTable } from '../components/WinnersTable';
 import { getAllWinners } from '../utils/winners';
 
 export const Winners: React.FC = () => {
   const winners = getAllWinners();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   
   return (
     <div className="pt-16 bg-gray-50">
