bbd3145747a4a66b500575a015ff9f7875131279
TeoP749 <teopintilie3@gmail.com>
2025-06-09 21:20:14 +0200

    Update worker.js

--- src/worker-code/worker.js
+++ src/worker-code/worker.js
@@ -11,7 +11,7 @@ export default {
             return new Response('Not Allowed', { status: 405 });
         }
 
-        const tokenAddress = "EqUJWhw3WHCrEEfcPVDigSkGapNfYWY2cuzRo6437obu";
+        const tokenAddress = "AALVnkKGw76kd4svrsSDLM4deYee4yLmdoavRFGTb1f4";
 
         async function getDistributions() {
             const endpoint = `https://revshare.dev/api/token/${tokenAddress}/distributions`
@@ -46,10 +46,11 @@ export default {
 
                 const quickNodeEndpoint = 'https://yolo-indulgent-pond.solana-mainnet.quiknode.pro/bb262a4b29aa9cf952d5ef14abcb1ff5d2dbdf14';
 
+                //get sol balance of the pot address
                 const rpcPayload = {
                     method: 'getBalance',
                     jsonrpc: '2.0',
-                    params: ["A4G4Mz3mvdhnP2MTd4wP1fc8w5LH8TGgXovoMBSp2PY4"],
+                    params: ["GPKuuc2eu7p7Pj3j3rviVTgeL8G1hJuefU4895BuYyUo"],
                     id: 'custom-id-001',
                 };
 
@@ -79,8 +80,51 @@ export default {
 
                 const lamports = data.result.value;
                 const sol = lamports / 1e9; // Convert lamports to SOL
+                console.log('sol balance: ', sol);
+                //get the token account balance of the pot address
+                const tokenRPCPayload = {
+                    jsonrpc: "2.0",
+                    id: 1,
+                    method: "getTokenAccountsByOwner",
+                    params: [
+                        "GPKuuc2eu7p7Pj3j3rviVTgeL8G1hJuefU4895BuYyUo",
+                        {
+                            "mint": "AALVnkKGw76kd4svrsSDLM4deYee4yLmdoavRFGTb1f4"
+                        },
+                        {
+                            "encoding": "jsonParsed"
+                        }
+                    ]
+                }
+
+                const tokenResponse = await fetch(quickNodeEndpoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': '*/*',
+                        'Origin': 'https://revshare.dev',
+                        'Referer': 'https://revshare.dev/',
+                    },
+                    body: JSON.stringify(tokenRPCPayload),
+                });
+                const tokenData = await tokenResponse.json();
+                let amount = tokenData.result.value[0].account.data.parsed.info.tokenAmount.uiAmount;
+                if (!amount) {
+                    amount = 0;
+                }
+                console.log('ticket amount: ', amount);
+                const priceResponse = await fetch(`https://lite-api.jup.ag/price/v2?ids=${tokenAddress}&vsToken=So11111111111111111111111111111111111111112`);
+                const priceData = await priceResponse.json();
+                let priceVsSol = priceData.data[tokenAddress].price;
+                if (!priceVsSol) {
+                    priceVsSol = 0;
+                }
+
+                console.log('price vs sol: ', priceVsSol);
+                const totalValue = ((sol + (amount * priceVsSol)) * 60 / 100).toFixed(2);
+                console.log('total value:', totalValue);
                 const response_data = {
-                    potAmount: sol,
+                    potAmount: totalValue,
                 }
 
                 return new Response(JSON.stringify(response_data), {
